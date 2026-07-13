e720c796a17920254e00142fbfbd84ba96ccda1a
Shughar-cyber <ayoifeomoyemi>
2026-07-13 16:57:53 -0500

    frontend URL added

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 const corsOptions = {
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "https://projectfrontend-one.vercel.app/",
     methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
     credentials: true
 }
